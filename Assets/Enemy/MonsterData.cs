9cea8dd8a46c2d4b42806630f239d9498724f189
jperez1010 <goldengamer760@gmail.com>
2024-02-24 13:19:15 -0800

    Maybe Changes

--- Assets/Enemy/MonsterData.cs
+++ Assets/Enemy/MonsterData.cs
@@ -5,5 +5,23 @@ using FOW;
 
 public class MonsterData : MonoBehaviour
 {
-    protected string name;
+    public Material material;
+
+    public void ActivateIdle()
+    {
+        // Create a new RGBA color using the Color constructor and store it in a variable
+        Color customColor = new Color(1f, 0f, 0f, 1.0f);
+
+        // Call SetColor using the shader property name "_Color" and setting the color to the custom color you created
+        material.color = customColor;
+    }
+
+    public void ActivateChase()
+    {
+        // Create a new RGBA color using the Color constructor and store it in a variable
+        Color customColor = new Color(1f, 1f, 1f, 1.0f);
+
+        // Call SetColor using the shader property name "_Color" and setting the color to the custom color you created
+        material.color = customColor;
+    }
 }
